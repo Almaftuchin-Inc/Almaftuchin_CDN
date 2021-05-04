fd41a2b9b19f7e1631d74e0e9e402f7a8625238e
✔Almaftuchin <me@almaftuchin.com>
2021-05-04 20:00:59 +0700

    Create block.js

---
+++ js/block.js
@@ -0,0 +1,40 @@
+if (ip == "114.122.106.15") {
+alert('You has been banned from our store!');
+window.location = 'https://shopee.co.id/almaftuchin';
+}
+if (ip == "114.124.236.75") {
+alert('You has been banned from our store!');
+window.location = 'https://shopee.co.id/almaftuchin';
+}
+if (ip == "36.95.15.36") {
+alert('You has been banned from our store!');
+window.location = 'https://shopee.co.id/almaftuchin';
+}
+if (ip == "116.197.128.193") {
+alert('You has been banned from our store!');
+window.location = 'https://shopee.co.id/almaftuchin';
+}
+if (ip == "119.2.52.141") {
+alert('You has been banned from our store!');
+window.location = 'https://shopee.co.id/almaftuchin';
+}
+if (ip == "114.125.170.239") {
+alert('You has been banned from our store!');
+window.location = 'https://shopee.co.id/almaftuchin';
+}
+if (ip == "190.2.146.231") {
+alert('You has been banned from our store!');
+window.location = 'https://shopee.co.id/almaftuchin';
+}
+if (ip == "125.160.64.159") {
+alert('You has been banned from our store!');
+window.location = 'https://shopee.co.id/almaftuchin';
+}
+if (ip == "195.181.172.74") {
+alert('You has been banned from our store!');
+window.location = 'https://shopee.co.id/almaftuchin';
+}
+if (ip == "114.122.132.88") {
+alert('You has been banned from our store!');
+window.location = 'https://shopee.co.id/almaftuchin';
+}
