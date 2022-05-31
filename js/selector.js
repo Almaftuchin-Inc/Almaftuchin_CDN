4682f5d9f71f78e6bfa79405691986bd8e529bb2
Almaftuchin✔ <me@almaftuchin.com>
2022-05-31 08:49:26 +0700

    Update selector.js

--- js/selector.js
+++ js/selector.js
@@ -7,4 +7,4 @@ function downloadJSAtOnload(){var e=document.createElement("script");e.src="http
 // Light Gallery
 $('#animated-thumbnail').lightGallery({thumbnail:true,getCaptionFromTitleOrAlt:true,selector:"a[style]"});
 // Lazy Related Post
-var lazyrelate=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyrelate||0!=document.body.scrollTop&&!1===lazyrelate)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://cdn.jsdelivr.net/gh/Arlina-Design/phantom@master/relatedzgn.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyrelate=!0)},!0);
+var lazyrelate=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyrelate||0!=document.body.scrollTop&&!1===lazyrelate)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://cdn.almaftuchin.com/js/related.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyrelate=!0)},!0);
