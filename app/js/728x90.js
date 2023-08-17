2362e6475464bccd52dccaa2d3c13e2a095ca87b
Almaftuchin✔ <me@almaftuchin.com>
2023-08-17 17:57:16 +0700

    Create 728x90.js

---
+++ app/js/728x90.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '022404ae0ec9fa0866ed2e9f17b6a3c4',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitablecreativeformat.com/022404ae0ec9fa0866ed2e9f17b6a3c4/invoke.js"></scr' + 'ipt>');
